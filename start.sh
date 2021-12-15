f243d215c62e4b32e80fe1f821e407ff5f8c5d42
walterpaulo <walter0paulo@hotmail.com>
2021-12-14 23:55:33 -0300

    correção de código.

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ DB=produtos.json
 echo -e "$JSON"
 
 
-(which json-servers > /dev/null 2>&1 && 
+(which json-server > /dev/null 2>&1 && 
         echo -e "API de Carros rodando\nArqueiroVerde" && 
 	json-server --watch $DB -p 8900) || 
 	echo -e "Instalar o Json-Server\n\nnpm install -g json-server\n"
